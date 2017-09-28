a8560380242df6ae4f92957bef9108acbc52357f
Chris Rohrer <chris.rohrer@ubuntunet.net>
2017-09-28 10:44:38 +0200

    Commit it all

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
+
+  AFFILIATIONS = {
+    member: "Member",
+    faculty: "Faculty",
+    student: "Student",
+    alum: "Alumnus",
+    affiliate: "Affiliate",
+    'library-walk-in': "Library Walk-In"
+  }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
+
+  validates :username, presence: true, uniqueness: true
+  validates :affiliation, presence: true
+  validates :affiliation,
+    inclusion: { in: AFFILIATIONS.stringify_keys.keys,
+    message: "%{value} is not a valid affiliation" }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "valid user" do
+    user = users(:dave)
+    assert user.valid?
+  end
+
+  test "invalid without username" do
+    user = User.new(email: 'john@examle.net', password: 'secret')
+    refute user.valid?, 'user is valid without a username'
+    assert_not_nil user.errors[:username], 'no validation error for username present'
+  end
+
+  test "invalid without email" do
+    user = User.new(username: 'john', password: 'secret', affiliation: 'member')
+    refute user.valid?, 'user is valid without an email'
+    assert_not_nil user.errors[:email], 'no validation error for email present'
+  end
+
+  test "invalid without password" do
+    user = User.new(username: 'john', email: 'secret', affiliation: 'member')
+    refute user.valid?, 'user is valid without a password'
+    assert_not_nil user.errors[:password], 'no validation error for password present'
+  end
+
+  test "invalid without affiliation" do
+    user = User.new(username: 'john', password: 'secret', email: 'john@example.ac.net')
+    refute user.valid?, 'user is valid without an affiliation'
+    assert_not_nil user.errors[:affiliation], 'no validation error for affiliation present'
+  end
+
+  test "invalid with unknown affiliation" do
+    user = User.new(username: 'john', password: 'secret', email: 'john@example.ac.net', affiliation: 'staff')
+    refute user.valid?, 'user is valid with unspecified affiliation'
+    assert_not_nil user.errors[:affiliation], 'no validation error for affiliation present'
+  end
+
+  test "invalid with already existing username" do
+    existing_user = users(:dave)
+    user = User.new(username: 'daveh', password: 'secret', email: 'john@example.ac.net', affiliation: 'staff')
+    refute user.valid?, 'user is valid with non-unique username'
+    assert_not_nil user.errors[:username], 'no validation error for username present'
+  end
+
 end
